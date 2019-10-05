22ce0265ff31c71db298adc6f2f89e67562f7473
Chai888 <fennychai88@gmail.com>
2019-10-05 12:16:21 +0800

    Change Onclick

--- app/src/main/java/com/newera/notetaker/MainActivity.java
+++ app/src/main/java/com/newera/notetaker/MainActivity.java
@@ -3,12 +3,39 @@ package com.newera.notetaker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText uname,pass;
+    private Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void findv(){
+        uname=findViewById(R.id.un);
+        pass=findViewById(R.id.p);
+        btnlogin=findViewById(R.id.btnl);
+    }
+    private void setLis(){
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputun=uname.getText().toString();
+                String inputp=pass.getText().toString();
+
+                if(inputun.equals(uname) && inputp.equals(pass)){
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT);
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT);
+                }
+
+            }
+        });
     }
 }
